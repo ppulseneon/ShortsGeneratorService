b21ac4c710d9e630bf81fe95c83f01a50e0ae30e
pulseneon <thebooom@yandex.ru>
2024-09-27 17:09:04 +0300

    Identified the routes and settings

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
-from flask import Flask
+from flask import Flask, request, jsonify, redirect
+
+from settings.static_files import fonts_list, format_types_list, primary_videos, music_list
+from settings.swagger_settings import swagger_ui_blueprint, SWAGGER_URL
 
 app = Flask(__name__)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
-
+"""
+Редирект с главной на Swagger 
+"""
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect("/swagger", code=302)
 
+"""
+Получить Short из видео
+"""
+@app.route('/video', methods=['POST'])
+def prepare_video():
+    return jsonify({"status": "ok", "result": "<insert url>"}), 200
 
-if __name__ == '__main__':
-    app.run()
+"""
+Получить доступные форматы видео
+"""
+@app.route('/video/format_type', methods=['GET'])
+def get_format_type():
+    return jsonify(format_types_list), 200
+
+"""
+Получить доступные шрифты для субтитров
+"""
+@app.route('/video/subtitles/fonts', methods=['GET'])
+def get_fonts():
+    return jsonify(fonts_list), 200
+
+"""
+Получить доступные завлекающие видео
+"""
+@app.route('/video/primary_videos', methods=['GET'])
+def get_fonts():
+    return jsonify(primary_videos), 200
+
+"""
+Получить доступную фоновую музыку
+"""
+@app.route('/video/music', methods=['GET'])
+def get_fonts():
+    return jsonify(music_list), 200
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ settings/static_files.py
@@ -0,0 +1,53 @@
+format_types_list = [
+    {
+        "format_type": 0,
+        "description": "Полноразмерное видео по центру с черным фоном",
+        "preview": "https://i.imgur.com/uN1BnAx.png"
+    },
+    {
+        "format_type": 1,
+        "description": "Увеличенное по центру с черным фоном",
+        "preview": "https://i.imgur.com/qOg2SgW.png"
+    },
+    {
+        "format_type": 2,
+        "description": "Полноразмерное по центру с заблюренным фоном",
+        "preview": "https://i.imgur.com/ZlEjaqG.png"
+    },
+    {
+        "format_type": 3,
+        "description": "Увеличенное по центру с заблюренным фоном",
+        "preview": "https://i.imgur.com/l0pSP56.png"
+    },
+    {
+        "format_type": 4,
+        "description": "Центр горизонтального видео",
+        "preview": "https://i.imgur.com/yhaVas6.png"
+    },
+    {
+        "format_type": 5,
+        "description": "Видео сверху экрана, где снизу видео для захвата внимания",
+        "preview": "https://i.imgur.com/oxXWYZN.png"
+    },
+    {
+        "format_type": 6,
+        "description": "Видео снизу экрана, где сверху видео для захвата внимания",
+        "preview": "https://i.imgur.com/qjEj0ng.png"
+    },
+]
+
+fonts_list = [
+    {
+        "font": "Roboto-Bold",
+        "font-family": "Roboto",
+        "font-weight": "700",
+    }
+]
+
+music_list = [
+
+]
+
+primary_videos = [
+
+]
